a7cfa3dc47453a70094498fc753185ed22b6061f
ABID JEEM <119529522+ajeem2026@users.noreply.github.com>
2024-03-23 09:51:14 -0400

    Update bits.c

--- datalab_Abid/bits.c
+++ datalab_Abid/bits.c
@@ -1,7 +1,8 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * Author: Abid Jeem'26
+ * Date: March, 2024
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
